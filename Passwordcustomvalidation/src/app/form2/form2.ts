6d579417d775e5a1d5d5a3f9fd2c2f769c3f1ec4
AbhijeetMulik1234 <mulikabhijeet948@gmail.com>
2025-08-07 17:29:04 +0530

    code check in
    angular demo

--- Passwordcustomvalidation/src/app/form2/form2.ts
+++ Passwordcustomvalidation/src/app/form2/form2.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-form2',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterOutlet, NgModule],
   templateUrl: './form2.html',
-  styleUrl: './form2.css'
+  styleUrl: './form2.css',
 })
 export class Form2 {
+  RegistrationForm: FormGroup;
+  accountTypes = ['Minor Account', 'Adult Account'];
+  Gender = ['Male', 'Female'];
+  interests = {
+    sports: false,
+    music: false,
+    travel: false,
+    reading: false,
+  };
 
+  constructor(private fb: FormBuilder) {
+    this.RegistrationForm = this.fb.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      dateofbirth: ['', [Validators.required]],
+      age: ['', Validators.required],
+      accountTypes: ['', Validators.required],
+      gender: ['', Validators.required],
+      interests: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.RegistrationForm.valid) {
+      console.log('Form Submitted:', this.RegistrationForm.value);
+      alert('Registration Successful');
+    } else {
+      alert('please fill all the required field');
+    }
+  }
 }
