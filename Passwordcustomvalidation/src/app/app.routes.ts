1ac337ca9791deded1e5606559599a36aee19c2a
AbhijeetMulik1234 <mulikabhijeet948@gmail.com>
2025-08-07 12:29:02 +0530

    code check in
    angular demo

--- Passwordcustomvalidation/src/app/app.routes.ts
+++ Passwordcustomvalidation/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Form1 } from './form1/form1';
+import { Form2 } from './form2/form2';
 
 export const routes: Routes = [
   {
     path: 'form1',
     component: Form1,
   },
+  { path: 'form2', component: Form2 },
 ];
--- Passwordcustomvalidation/src/app/app.ts
+++ Passwordcustomvalidation/src/app/app.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { calculateAge } from './Validators/age-calculation.validator';
 import { CommonModule } from '@angular/common';
 import { Form1 } from './form1/form1';
+import { Form2 } from './form2/form2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, Form1],
+  imports: [RouterOutlet, CommonModule, Form1, Form2],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
