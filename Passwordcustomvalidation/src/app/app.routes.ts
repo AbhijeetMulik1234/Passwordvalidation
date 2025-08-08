3750dea1c8951e7a6eb09162f952ec8f1e876fe3
AbhijeetMulik1234 <mulikabhijeet948@gmail.com>
2025-08-08 20:03:34 +0530

    code check in
    angular demo

--- Passwordcustomvalidation/src/app/app.routes.ts
+++ Passwordcustomvalidation/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Form1 } from './form1/form1';
 import { Form2 } from './form2/form2';
+import { Form3 } from './form3/form3';
 
 export const routes: Routes = [
   {
@@ -8,4 +9,5 @@ export const routes: Routes = [
     component: Form1,
   },
   { path: 'form2', component: Form2 },
+  { path: 'form3', component: Form3 },
 ];
--- Passwordcustomvalidation/src/app/form3/form3.ts
+++ Passwordcustomvalidation/src/app/form3/form3.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form3',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form3.html',
-  styleUrl: './form3.css'
+  styleUrl: './form3.css',
 })
 export class Form3 {
+  Userform: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.Userform = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(3)]],
+      lastname: ['', [Validators.required, Validators.maxLength(12)]],
+      email: ['', [Validators.required, Validators.email]],
+      adress: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        pinCode: ['', Validators.required],
+      }),
+      contact: this.fb.group({
+        phoneno: ['', Validators.required],
+        alternativephoneno: ['', Validators.required],
+        emergencycontactname: ['', Validators.required],
+        emergencycontactnumber: ['', Validators.required],
+      }),
+      onSubmit() {
+        console.log('Form Submitted');
+      },
+    });
+  }
 }
